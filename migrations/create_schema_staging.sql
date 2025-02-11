3598866ecb093efc6f85a1cdf437ff843f6d59fc
zoneout215 <the.red.smoke420@gmail.com>
2025-02-12 00:28:07 +0100

    try to fix ports in clickhouse container and change schema for hadoop -> ch migration

--- migrations/create_schema_staging.sql
+++ migrations/create_schema_staging.sql
@@ -1,31 +1,36 @@
 CREATE OR REPLACE VIEW default.staging_metadata
 AS
     SELECT
-        asin,
-        categories,
-        description,
-        title,
-        price,
-        salesRank as sales_rank,
-        imUrl as im_url,
-        related,
+        JSONExtractString(m.json_string, 'asin') AS asin,
+        arrayJoin(
+            arrayJoin(
+                JSONExtract(
+                    json_string, 'categories', 'Array(Array(String))')
+                )) as categories,
+        JSONExtractString(m.json_string, 'description') AS description,
+        JSONExtractString(m.json_string, 'title') AS title,
+        JSONExtractFloat(m.json_string, 'price') AS price,
+        JSONExtractInt(m.json_string, 'salesRank') AS salesRank,
+        JSONExtractString(m.json_string, 'imUrl') AS imUrl,
+        JSONExtractString(m.json_string, 'related') AS related,
         created_at,
     FROM
-        default.raw_data_metadata AS m;
+        default.raw_data_metadata AS m
+    LIMIT 10;
 
 CREATE OR REPLACE VIEW default.staging_items
 AS 
     SELECT
-        reviewerID,
-        asin,
-        reviewerName,
-        helpful,
-        reviewText,
-        overall as rating,
-        summary,
-        unixReviewTime,
-        reviewTime,
+        JSONExtractString(r.json_string, 'reviewerID') AS reviewerID,
+        JSONExtractString(r.json_string, 'asin') AS asin,
+        JSONExtractString(r.json_string, 'reviewerName') AS reviewerName,
+        JSONExtractInt(r.json_string, 'helpful') AS helpful,
+        JSONExtractString(r.json_string, 'reviewText') AS reviewText,
+        JSONExtractFloat(r.json_string, 'overall') AS rating,
+        JSONExtractString(r.json_string, 'summary') AS summary,
+        JSONExtractInt(r.json_string, 'unixReviewTime') AS unixReviewTime,
+        JSONExtractString(r.json_string, 'reviewTime') AS reviewTime,
         created_at
     FROM
-        default.raw_data_items AS r;
-
+        default.raw_data_items AS r
+    LIMIT 10;
