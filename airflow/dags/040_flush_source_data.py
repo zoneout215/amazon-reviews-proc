047ab5886c4438edc5c07011960a18883a79647c
zoneout215 <the.red.smoke420@gmail.com>
2025-02-12 21:05:05 +0100

    add dag for regular source data offload

---
+++ airflow/dags/040_flush_source_data.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from plugins.flush_source_data import flush_metadata, flush_items
+from airflow import DAG
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+
+
+DEFAULT_ARGS = {
+    'owner': 'sergei.romanov',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
+dag_config = {
+    'dag_id': '030_dbt_processing',
+    'default_args': DEFAULT_ARGS,
+    'description': 'An Airflow DAG to flush files to local storage',
+    'schedule_interval': '0 * * * *',
+    'max_active_runs': 1,
+    'catchup': False,
+    'start_date': datetime(2025, 2, 14),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+with DAG(**dag_config) as dag:
+
+    start = DummyOperator(task_id='start')
+    end = DummyOperator(task_id='end')
+    flush_metadata = PythonOperator(
+        task_id='flush_metadata',
+        python_callable=flush_metadata,
+    )
+
+    flush_items = PythonOperator(
+        task_id='flush_items',
+        python_callable=flush_items
+    )
+
+    start >>[flush_items, flush_metadata] >> end
