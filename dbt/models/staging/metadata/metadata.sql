d49c8e136a452380a860ab235d518a3859958d87
zoneout215 <the.red.smoke420@gmail.com>
2025-02-12 21:08:50 +0100

    add models of stag layer with json parsing

---
+++ dbt/models/staging/metadata/metadata.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='view',
+        alias='metadata'
+    )
+}}
+
+SELECT
+    m.asin,
+    m.categories,
+    m.description,
+    m.title,
+    m.price,
+    m.salesRank as sales_rank,
+    m.imUrl as im_url,
+    m.related,
+    m.created_at,
+FROM
+    {{ source('default', 'staging_metadata') }} AS m
+LIMIT 10
