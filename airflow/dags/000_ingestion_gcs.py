de377a343095df07ca5346e2884bc91fecd2aaf7
zoneout215 <the.red.smoke420@gmail.com>
2025-03-18 21:32:52 +0100

    create a dag for ingestion on gcs

--- airflow/dags/000_ingestion_gcs.py
+++ airflow/dags/000_ingestion_gcs.py
@@ -3,35 +3,17 @@ from datetime import datetime, timedelta, timezone, date
 from airflow.providers.google.cloud.operators.cloud_storage_transfer_service import (
     CloudDataTransferServiceCreateJobOperator
 )
-from airflow.operators.bash_operator import BashOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.models.baseoperator import chain 
 from plugins.ingest import parse_date, parse_time
 
 GCP_PROJECT_ID = 'e-analogy-449921-p7'
-URL_LIST = "gs://jet-assignment-211312/test/test.tsv" 
-METADATA_LINK ="https://snap.stanford.edu/data/amazon/productGraph/metadata.json.gz"
-RATINGS_LINK =  "https://snap.stanford.edu/data/amazon/productGraph/item_dedup.json.gz"
-BUCKET_NAME = 'jet-assignment-12312'
-DESTINATION_PATH_PREFIX = 'landing/'  # Optional path prefix in the bucket
-
-DEFAULT_ARGS = {
-    'owner': 'sergei.romanov',
-    'clickhouse_conn_id': 'clickhouse',
-    'database': 'default',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False
-}
-
-dag_config = {
-    'dag_id': '000_ingerstion_gcs',
-    'default_args': DEFAULT_ARGS,
-    'schedule_interval': None,
-    'max_active_runs': 1,
-    'catchup': False,
-    'start_date': datetime.now(timezone.utc)  
-}
+URL_LIST = f"gs://bucket_amazon_reviews/landing/data_sources.tsv" 
+BUCKET_NAME = 'bucket_amazon_reviews'
+DESTINATION_PATH_PREFIX = 'landing/'  
+SOURCES_LINKS_DIR = "/opt/airflow/data_sources.tsv"
 
 SCHEDULE = {
         "scheduleStartDate":  parse_date(datetime.now(timezone.utc).date().strftime("%Y-%m-%d")),
@@ -39,13 +21,7 @@ SCHEDULE = {
         "startTimeOfDay": parse_time((datetime.now(tz=timezone.utc) + timedelta(minutes=1)).time().strftime("%H:%M:%S")),
     }
 
-with DAG(**dag_config) as dag:
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')   
-    create_transfer = CloudDataTransferServiceCreateJobOperator(
-        task_id='create_transfer_job',
-        project_id=GCP_PROJECT_ID,
-        body={
+TRANSFER_BODY = {
             'description': 'Transfer data from public URL to GCS',
             'status': 'ENABLED',
             'projectId': GCP_PROJECT_ID,
@@ -60,5 +36,53 @@ with DAG(**dag_config) as dag:
                 }
             },
         }
+
+DEFAULT_ARGS = {
+    'owner': 'sergei.romanov',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
+dag_config = {
+    'dag_id': '000_ingerstion_gcs',
+    'default_args': DEFAULT_ARGS,
+    'schedule_interval': None,
+    'max_active_runs': 1,
+    'catchup': False,
+    'start_date': datetime.now(timezone.utc)  
+}
+
+
+
+with DAG(**dag_config) as dag:
+    start = DummyOperator(task_id='start')
+    end = DummyOperator(task_id='end')   
+
+    upload_task = LocalFilesystemToGCSOperator(
+        task_id='upload_file',
+        src=SOURCES_LINKS_DIR,  
+        dst=DESTINATION_PATH_PREFIX,  
+        bucket=BUCKET_NAME, 
     )
-    chain(start, create_transfer, end)
+
+    create_transfer = CloudDataTransferServiceCreateJobOperator(
+        task_id='create_transfer_job',
+        project_id=GCP_PROJECT_ID,
+        body=TRANSFER_BODY
+    )
+
+    sensor_task_metadata = GCSObjectExistenceSensor(
+        task_id='sensor_task_metadata',
+        bucket=BUCKET_NAME,
+        object=f'{BUCKET_NAME}/landing/snap.stanford.edu/data/amazon/productGraph/metadata.json.gz',
+        deferrable=True,  # Optional: Run in deferrable mode to free up worker slots
+    )
+
+    sensor_task_items = GCSObjectExistenceSensor(
+        task_id='sensor_task_items',
+        bucket=BUCKET_NAME,
+        object=f'{BUCKET_NAME}/landing/snap.stanford.edu/data/amazon/productGraph/item_dedup.json.gz',
+        deferrable=True,  # Optional: Run in deferrable mode to free up worker slots
+    )
+    chain(start,upload_task, create_transfer, [sensor_task_metadata, sensor_task_items], end)
