a086b24d3e2f0d024447f2e6381766b653504365
zoneout215 <the.red.smoke420@gmail.com>
2025-03-22 22:53:39 +0100

    remove redundant files

--- migrations/create_schema_raw_data.sql
+++
@@ -1,9 +0,0 @@
--- Converted to BigQuery DDL from ClickHouse
-CREATE OR REPLACE TABLE `amazon_reviews.raw_metadata_gcs` (
-    json_string STRING,
-    created_at DATE
-);
-
-CREATE OR REPLACE TABLE `amazon_reviews.raw_items_gcs` (
-    
-);
